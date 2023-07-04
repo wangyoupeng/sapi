b6f0a8903b68a965b2f0d65c0d2dd7774c76a68b
wyp <youpeng.wang@marsmarch.cn>
2023-07-04 10:46:44 +0800

    f

--- app/middleware/rateLimit.js
+++ app/middleware/rateLimit.js
@@ -1,6 +1,9 @@
 const rateLimit = require("koa-ratelimit");
 // 限制每分钟最多 100 个请求
+const db = new Map();
 rateLimit({
+  driver: 'memory',
+  db: db,
   duration: 1000, // 设置限流的时间段，单位为毫秒
   max: 3, // 设置时间段内最多允许的请求数量
   message: "请求过于频繁，请稍后再试", // 超过限制的提示信息
